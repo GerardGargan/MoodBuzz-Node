9a70f12d6ffd5dd9dcc0e7e193295e1977a5243f
Gerard Gargan <gerardgargan@hotmail.co.uk>
2024-01-11 22:36:38 +0000

    created database connection to mysql and imported to router file for use

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const path = require('path');
+const db = require('../util/dbconn');
 
 const router = express.Router();
 
-router.get('', (req, res) => {
-    res.send('Hello world');
+router.get('/', (req, res) => {
+    res.send('<h1>Hello World</h1>');
 })
 
 module.exports = router;
