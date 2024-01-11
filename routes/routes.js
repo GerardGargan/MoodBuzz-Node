280fa90e39927a518ea5022a19cbe06f49565397
Gerard Gargan <gerardgargan@hotmail.co.uk>
2024-01-11 22:59:15 +0000

    set up some test queries and sent the json result in the response

--- routes/routes.js
+++ routes/routes.js
@@ -4,8 +4,35 @@ const db = require('../util/dbconn');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>');
-})
+router.get('/', async (req, res) => {
+    try {
+        const query1 = 'SELECT * FROM emotion';
+        const query2 = 'SELECT * FROM triggers';
+
+        const result1 = await executeQuery(query1);
+        const result2 = await executeQuery(query2);
+
+        const combinedResults = {result1, result2};
+
+        res.send(combinedResults);
+
+    } catch(error) {
+        console.log(error);
+    }
+
+    
+});
+
+function executeQuery(query) {
+    return new Promise((resolve, reject) => {
+        db.query(query, (err, rows) => {
+            if(err) {
+                reject(err);
+            } else {
+                resolve(rows);
+            }
+        });
+    });
+}
 
 module.exports = router;
