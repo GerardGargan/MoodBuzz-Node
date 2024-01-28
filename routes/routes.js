d6b4f1a46ee1c251c4b2a3f7d4ecf637959b96a7
Gerard Gargan <gerardgargan@hotmail.co.uk>
2024-01-28 14:55:40 +0000

    added view snapshot page and server side logic

--- routes/routes.js
+++ routes/routes.js
@@ -11,6 +11,7 @@ router.post('/register', controller.postRegister);
 router.get('/user/home', controller.getUserHomePage);
 router.get('/user/snapshot', controller.getNewSnapshotPage);
 router.get('/new/snapshot', controller.processNewSnapshot);
+router.get('/user/snapshot/view/:id', controller.getViewSnapshot);
 router.get('*', controller.getNotFound);
 
 module.exports = router;
