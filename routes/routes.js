377d209cb98d923f4f5fcd331a1479dda95abeb2
Gerard Gargan <gerardgargan@hotmail.co.uk>
2024-01-29 15:37:27 +0000

    added logic and SQL for deleting a snapshot

--- routes/routes.js
+++ routes/routes.js
@@ -12,6 +12,7 @@ router.get('/user/home', controller.getUserHomePage);
 router.get('/user/snapshot', controller.getNewSnapshotPage);
 router.get('/new/snapshot', controller.processNewSnapshot);
 router.get('/user/snapshot/view/:id', controller.getViewSnapshot);
+router.get('/user/snapshot/del/:id', controller.deleteSnapshot);
 router.get('*', controller.getNotFound);
 
 module.exports = router;
