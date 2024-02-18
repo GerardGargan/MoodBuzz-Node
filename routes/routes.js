f7f47eb2780c710374faa052c1b2cb2de0ae5faa
Gerard Gargan <gerardgargan@hotmail.co.uk>
2024-02-18 11:31:40 +0000

    created middleware for authentication and used on routers

--- routes/routes.js
+++ routes/routes.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const controller = require('./../controllers/controller');
-
+const { isAuth, isLoggedIn } = require('../middleware/middleware');
 const router = express.Router();
 
 router.get('/', controller.getIndex);
-router.get('/login', controller.getLogin);
-router.post('/login', controller.postLogin);
-router.get('/register', controller.getRegister);
-router.post('/register', controller.postRegister);
-router.get('/user/home', controller.getUserHomePage);
-router.get('/user/snapshot', controller.getNewSnapshotPage);
-router.post('/user/snapshot', controller.processNewSnapshot);
-router.get('/user/snapshot/view/:id', controller.getViewSnapshot);
-router.get('/user/snapshot/del/:id', controller.deleteSnapshot);
+router.get('/login', isLoggedIn, controller.getLogin);
+router.get('/register', isLoggedIn, controller.getRegister);
+router.get('/user/home', isAuth, controller.getUserHomePage);
+router.get('/user/snapshot', isAuth, controller.getNewSnapshotPage);
+router.get('/user/snapshot/view/:id', isAuth, controller.getViewSnapshot);
+router.get('/user/snapshot/del/:id', isAuth, controller.deleteSnapshot);
 router.get('/user/logout', controller.getLogout);
-router.get('/user/snapshot/edit/:id', controller.getEdit);
-router.post('/user/snapshot/update/:id', controller.postEditUpdate);
+router.get('/user/snapshot/edit/:id', isAuth, controller.getEdit);
+router.post('/register', isLoggedIn, controller.postRegister);
+router.post('/login', isLoggedIn, controller.postLogin);
+router.post('/user/snapshot', isAuth, controller.processNewSnapshot);
+router.post('/user/snapshot/update/:id', isAuth, controller.postEditUpdate);
 router.get('*', controller.getNotFound);
 
 module.exports = router;
